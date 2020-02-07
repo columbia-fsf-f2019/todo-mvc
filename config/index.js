157be398052408a5afa622d3b897f71af5b48a5f
russomp <russo.michaelp@gmail.com>
2020-02-06 21:55:44 -0500

    chore: Connect to heroku db in prod

--- config/index.js
+++ config/index.js
@@ -1,11 +1,13 @@
 var mysql = require('mysql');
 
-var connection = mysql.createConnection({
-  host: 'localhost',
-  port: 3306,
-  user: 'root',
-  password: '1234',
-  database: 'todos_db'
-});
+var connection = mysql.createConnection(
+  process.env.JAWSDB_URL || {
+    host: 'localhost',
+    port: 3306,
+    user: 'root',
+    password: '1234',
+    database: 'todos_db'
+  }
+);
 
 module.exports = connection;
