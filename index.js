a2263018298d4fc6a3b1bdce347623dac0d88970
russomp <russo.michaelp@gmail.com>
2020-02-08 13:01:16 -0500

    feat: Build out views

--- index.js
+++ index.js
@@ -1,13 +1,29 @@
 const express = require('express');
 const morgan = require('morgan');
+const exphbs = require('express-handlebars');
+const List = require('./models/list');
+
+var app = express();
+
+app.engine('handlebars', exphbs());
+app.set('view engine', 'handlebars');
 
 const PORT = 8080;
 
-const app = express();
 app.use(express.json());
 app.use(express.urlencoded());
 app.use(morgan('dev'));
 app.use(require('./routes'));
+
+app.get('/lists', async (req, res) => {
+  const lists = await List.all();
+  res.render('lists', { lists });
+});
+
+app.get('/lists/:id', async (req, res) => {
+  const todos = await List.todos(req.params.id);
+  res.render('todolist', { todos });
+});
 
 app.listen(process.env.PORT || PORT, () => {
   console.log(`server running on ${process.env.PORT || PORT}`);
