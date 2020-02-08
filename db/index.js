127fbb7c0874200010164f080b1bf5143e2fe37a
russomp <russo.michaelp@gmail.com>
2020-02-08 11:32:15 -0500

    feat: Add CRUD methods to ORM and refactor todo routes

--- db/index.js
+++ db/index.js
@@ -2,19 +2,32 @@ var db = require('../config');
 
 const orm = {
   selectAll: async table => {
-    // db.query('SELECT * FROM todos;', (err, results) => {
-    //   if (err) res.status(500).json({ message: err });
-    //   res.json({ todos: results });
-    // });
     const data = await db.promise().query('SELECT * FROM ??;', [table]);
     return data || [];
   },
   selectOneById: async (table, id) => {
-    const [
-      data,
-      info
-    ] = await db.promise().query('SELECT * FROM ?? WHERE id = ?;', [table, id]);
+    const [data, _] = await db
+      .promise()
+      .query('SELECT * FROM ?? WHERE id = ?;', [table, id]);
     return data || [];
+  },
+  insertInto: async (table, data) => {
+    const result = await db
+      .promise()
+      .query(`INSERT INTO ?? SET ?`, [table, data]);
+    return result;
+  },
+  updateOneById: async (table, id, data) => {
+    const result = await db
+      .promise()
+      .query('UPDATE ?? SET ? WHERE id = ?;', [table, data, id]);
+    return result;
+  },
+  deleteOneById: async (table, id) => {
+    const result = await db
+      .promise()
+      .query('DELETE FROM ?? WHERE id = ?', [table, id]);
+    return result;
   }
 };
 
--- routes/api/todos.js
+++ routes/api/todos.js
@@ -2,48 +2,28 @@ var router = require('express').Router();
 var orm = require('../../db');
 
 router.get('/', async (req, res) => {
-  console.log('before');
   const todos = await orm.selectAll('todos');
-  console.log('after');
   res.json({ todos: todos });
 });
 
-// router.post('/', (req, res) => {
-//   console.log(req.body);
-//   db.query(
-//     `INSERT INTO todos (text) VALUES ("${req.body.text}");`,
-//     (err, results) => {
-//       if (err) res.status(500).json({ message: err.sqlMessage });
-
-//       res.send(results);
-//     }
-//   );
-// });
+router.post('/', async (req, res) => {
+  const result = await orm.insertInto('todos', req.body);
+  res.json(result);
+});
 
 router.get('/:id', async (req, res) => {
   const todo = await orm.selectOneById('todos', req.params.id);
   res.json(todo);
 });
 
-// router.put('/:id', (req, res) => {
-//   db.query(
-//     `UPDATE todos SET ? WHERE id = ${req.params.id};`,
-//     req.body,
-//     (err, results) => {
-//       if (err) res.status(500).json({ message: err.sqlMessage });
-//       if (results.affectedRows === 0) res.status(404).end();
+router.put('/:id', async (req, res) => {
+  const result = await orm.updateOneById('todos', req.params.id, req.body);
+  res.json(result);
+});
 
-//       res.json(results.changedRows);
-//     }
-//   );
-// });
-
-// router.delete('/:id', (req, res) => {
-//   db.query(`DELETE FROM todos WHERE id = ${req.params.id};`, (err, results) => {
-//     if (err) res.status(500).json({ message: err.sqlMessage });
-//     if (results.affectedRows === 0) res.status(404).end();
-//     res.json(results);
-//   });
-// });
+router.delete('/:id', async (req, res) => {
+  const result = await orm.deleteOneById('todos', req.params.id);
+  res.json(result);
+});
 
 module.exports = router;
